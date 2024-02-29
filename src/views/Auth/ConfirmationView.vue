b02888c1a04ae484f86897d02cc74eca4904f695
BishalAryal1067 <celestial@pop-os.localdomain>
2024-02-29 15:27:02 +0545

    merged

--- src/views/Auth/ConfirmationView.vue
+++ src/views/Auth/ConfirmationView.vue
@@ -7,11 +7,9 @@ import Icon from "@icon/Icon.vue"
   <div class="w-full min-h-screen flex flex-col gap-2 justify-center items-center">
     <Icon :fill="'blue_shade_2'"/>
     <h1 class="text-blue_shade_2 text-lg">Check your email for verification</h1>
-=======
   <div class="w-full min-h-screen flex flex-col gap-2 justify-center items-center bg-[black]">
     <Icon :fill="'blue_shade_2'"/>
     <h1 class="text-blue_shade_light text-lg">Check your email for verification</h1>
->>>>>>> Stashed changes
   </div>
 </template>
 
