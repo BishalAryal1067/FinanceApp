4aa240fb16f2de0abb324c640d601e3c03e92243
BishalAryal1067 <celestial@pop-os.localdomain>
2024-02-29 15:22:03 +0545

    Confirmation page background change

--- src/views/Auth/ConfirmationView.vue
+++ src/views/Auth/ConfirmationView.vue
@@ -3,9 +3,15 @@ import Certificate from '../../components/Icons/Certificate.vue';
 </script>
 
 <template>
+<<<<<<< Updated upstream
   <div class="w-full min-h-screen flex flex-col gap-2 justify-center items-center">
     <Certificate fill="blue_shade_2"/>
     <h1 class="text-blue_shade_2 text-lg">Check your email for verification</h1>
+=======
+  <div class="w-full min-h-screen flex flex-col gap-2 justify-center items-center bg-[black]">
+    <Icon :fill="'blue_shade_2'"/>
+    <h1 class="text-blue_shade_light text-lg">Check your email for verification</h1>
+>>>>>>> Stashed changes
   </div>
 </template>
 
