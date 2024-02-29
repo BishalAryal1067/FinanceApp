cbb5ec0c5f1810f974b4b7965a059b63fd29b1e0
BishalAryal1067 <celestial@pop-os.localdomain>
2024-02-29 20:09:54 +0545

    merge conflict fixed

--- src/views/Auth/ConfirmationView.vue
+++ src/views/Auth/ConfirmationView.vue
@@ -3,10 +3,6 @@ import Icon from "@icon/Icon.vue"
 </script>
 
 <template>
-<<<<<<< Updated upstream
-  <div class="w-full min-h-screen flex flex-col gap-2 justify-center items-center">
-    <Icon :fill="'blue_shade_2'"/>
-    <h1 class="text-blue_shade_2 text-lg">Check your email for verification</h1>
   <div class="w-full min-h-screen flex flex-col gap-2 justify-center items-center bg-[black]">
     <Icon :fill="'blue_shade_2'"/>
     <h1 class="text-blue_shade_light text-lg">Check your email for verification</h1>
